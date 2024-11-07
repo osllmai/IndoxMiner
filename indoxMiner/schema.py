abda73280ce8b1fd8f3aacdababd249581c28eba
unknown <ashkan.eskandari.dev@gmail.com>
2024-11-07 15:50:34 +0330

    fix invoice schema

--- indoxMiner/schema.py
+++ indoxMiner/schema.py
@@ -197,10 +197,9 @@ class Schema:
         fields=[
             Field(
                 name="Invoice Number",
-                description="Unique invoice identifier , may have '#' in start of this number",
-                field_type=FieldType.STRING,
+                description="Unique invoice identifier",
+                field_type=FieldType.INTEGER,
                 required=True,
-                rules=ValidationRule(pattern=r"^INV-\d{6}$"),
             ),
             Field(
                 name="Date",
@@ -209,13 +208,13 @@ class Schema:
                 required=True,
                 rules=CommonValidationRules.DATE_RULE,
             ),
-            Field(
-                name="Due Date",
-                description="Payment due date",
-                field_type=FieldType.DATE,
-                required=True,
-                rules=CommonValidationRules.DATE_RULE,
-            ),
+            # Field(
+            #     name="Due Date",
+            #     description="Payment due date",
+            #     field_type=FieldType.DATE,
+            #     required=True,
+            #     rules=CommonValidationRules.DATE_RULE,
+            # ),
             Field(
                 name="Company Name",
                 description="Name of the company issuing invoice",
@@ -224,32 +223,31 @@ class Schema:
                 rules=ValidationRule(min_length=2, max_length=100),
             ),
             Field(
-                name="Company Address",
+                name="Address",
                 description="Address of the company or ship to",
                 field_type=FieldType.STRING,
                 required=True,
-                rules=CommonValidationRules.ADDRESS_RULE,
             ),
-            Field(
-                name="Company Tax ID",
-                description="Tax ID or VAT number",
-                field_type=FieldType.STRING,
-                rules=ValidationRule(pattern=ValidationPatterns.VAT_NUMBER["eu"]),
-            ),
-            Field(
-                name="Customer Name",
-                description="Name of the customer",
-                field_type=FieldType.STRING,
-                required=True,
-                rules=CommonValidationRules.NAME_RULE,
-            ),
-            Field(
-                name="Customer Address",
-                description="Address of the customer",
-                field_type=FieldType.STRING,
-                required=True,
-                rules=CommonValidationRules.ADDRESS_RULE,
-            ),
+            # Field(
+            #     name="Company Tax ID",
+            #     description="Tax ID or VAT number",
+            #     field_type=FieldType.STRING,
+            #     rules=ValidationRule(pattern=ValidationPatterns.VAT_NUMBER["eu"]),
+            # ),
+            # Field(
+            #     name="Customer Name",
+            #     description="Name of the customer",
+            #     field_type=FieldType.STRING,
+            #     required=True,
+            #     rules=CommonValidationRules.NAME_RULE,
+            # ),
+            # Field(
+            #     name="Customer Address",
+            #     description="Address of the customer",
+            #     field_type=FieldType.STRING,
+            #     required=True,
+            #     rules=CommonValidationRules.ADDRESS_RULE,
+            # ),
             Field(
                 name="Items",
                 description="List of items in the invoice",
