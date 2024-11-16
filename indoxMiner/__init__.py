2a5725acbc8904abfc2c4c0a5db57a2f5cfcf184
unknown <ashkan.eskandari.dev@gmail.com>
2024-11-16 17:20:45 +0330

    ver 0.0.12

--- indoxMiner/__init__.py
+++ indoxMiner/__init__.py
@@ -41,6 +41,6 @@ __all__ = [
 ]
 
 # Package metadata
-__version__ = "0.0.0"
+__version__ = "0.0.12"
 __author__ = "IndoxMiner Team"
 __description__ = "A comprehensive document extraction and processing library"
